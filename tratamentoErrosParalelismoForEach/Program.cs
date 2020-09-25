f728cee21884b03fc43f8e8a331a586c186eff95
Rafael Eduardo Rauber <rrauber@bempromotora.com.br>
2020-09-25 12:48:43 -0300

    teste
    teste

---
+++ tratamentoErrosParalelismoForEach/Program.cs
@@ -0,0 +1,97 @@
+﻿using Servico.Jobs;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tratamentoErrosParalelismoForEach
+{
+
+    public class rauber
+    {
+       public string nome { get; set; }
+    }
+    public class JUCA: rauber
+    {
+        public string idade { get; set; }
+        public string cpf { get; set; }
+    }
+
+    public class Pires: rauber
+    {
+        public string tamanho { get; set; }
+        public string sobrenome { get; set; }
+
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+
+            var juca = new List<JUCA>();
+            juca.Add(new JUCA() { nome = "rauber", cpf = "123", idade = "10" });
+            juca.Add(new JUCA() { nome = "MOta", cpf = "321", idade = "10" });
+            juca.Add(new JUCA() { nome = "Pires", cpf = "147", idade = "10" });
+
+
+            var pires= new List<Pires>();
+            pires.Add(new Pires() { nome = "rauber",  tamanho= "123", sobrenome = "10" });
+            pires.Add(new Pires() { nome = "MOta", tamanho = "321", sobrenome = "10" });
+            pires.Add(new Pires() { nome = "Pires", tamanho = "147", sobrenome = "10" });
+
+
+            var task1 = Utilitarios.ExecutarGenerico(juca, Utilitarios.TipoOperacao.DesaverbacaoCartao, 5);
+            var task2 = Utilitarios.ExecutarGenerico(pires, Utilitarios.TipoOperacao.AverbacaoCreditoConsignado, 1);
+
+            await Task.WhenAll(task1, task2);
+
+            //string x = "dsds";
+            //string y = x;
+
+            //string? teste = null;
+
+            //int[] lista = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+
+            //Teste t = new Teste();
+            //t.Testar(lista);
+
+            //string[] ss = null;
+            //List<string> s = new List<string>();
+            //s.Add("");
+
+            //var xxx = s?.Any() ?? false;
+
+        }
+
+    }
+
+
+    public class Teste
+    {
+        public Task Testar(int[] lista)
+        {
+            var exceptions = new ConcurrentQueue<Exception>();
+
+            _ = Parallel.ForEach(lista, new ParallelOptions()
+            { MaxDegreeOfParallelism = 2 },
+              async (proposta) =>
+              {
+                  try
+                  {
+                      throw new Exception("dsadsadsa");
+                  }
+                  catch (Exception ex)
+                  {
+                      exceptions.Enqueue(ex);
+                  }
+              });
+
+            return Task.FromResult("OK");
+        }
+    }
+}
---
+++ tratamentoErrosParalelismoForEach/Utilitarios.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tratamentoErrosParalelismoForEach;
+
+namespace Servico.Jobs
+{
+    public static class Utilitarios
+    {
+        public enum TipoOperacao
+        {
+            AverbacaoCartao = 1,
+            AverbacaoCreditoConsignado = 2,
+            DesaverbacaoCartao = 3,
+            DesaverbacaoCreditoConsignado = 4,
+            metodoXPTO = 5,
+            criarNOovo = 6
+
+        }
+
+
+        public static async Task TesteRauber(object teste, string valor, TipoOperacao tipoOperacao)
+        {
+            Console.WriteLine(tipoOperacao);
+            Console.WriteLine(valor);
+            Console.WriteLine(teste.ToString());
+        }
+
+        public static Task ExecutarGenerico<T>(this IEnumerable<T> ListaProcessamento, TipoOperacao tipoOperacao, int quantidadeParalelismo) where T : rauber
+        {
+            string objeto = "RoboEcoBem.AverbacaoCartaoJob.Execute";
+            string mensagem = "Iniciou o job de Averbação cartão";
+            int etapa = 1;
+            bool erroExecucao = false;
+
+            try
+            {
+                if (!(ListaProcessamento?.Any() ?? false))
+                {
+                    return Task.FromResult("OK");
+                }
+
+                _ = Parallel.ForEach(ListaProcessamento, new ParallelOptions() { MaxDegreeOfParallelism = quantidadeParalelismo },
+                async (proposta) =>
+                {
+                    mensagem = null;
+                    try
+                    {
+                        var task1 = Task.FromResult(mensagem);
+
+                        Task task2 = null;
+                        switch (tipoOperacao)
+                        {
+                            case TipoOperacao.AverbacaoCartao:
+                                task2 = TesteRauber(proposta, proposta.nome, tipoOperacao);
+                                break;
+                            case TipoOperacao.AverbacaoCreditoConsignado:
+                                task2 = TesteRauber(proposta, proposta.nome, tipoOperacao);
+                                break;
+                            case TipoOperacao.DesaverbacaoCartao:
+                                task2 = TesteRauber(proposta, proposta.nome, tipoOperacao);
+                                break;
+                            case TipoOperacao.DesaverbacaoCreditoConsignado:
+                                task2 = TesteRauber(proposta, proposta.nome, tipoOperacao);
+                                break;
+                            case TipoOperacao.metodoXPTO:
+                                task2 = TesteRauber(proposta, proposta.nome, tipoOperacao);
+                                break;
+                            case TipoOperacao.criarNOovo:
+                                task2 = TesteRauber(proposta, proposta.nome, tipoOperacao);
+                                break;
+                            default:
+                                task2 = TesteRauber(proposta, proposta.nome, tipoOperacao);
+                                break;
+                        }
+
+
+                        await Task.WhenAll(task1, task2);
+
+                        mensagem = "Executou API Eco sem erro.";
+                    }
+                    catch (Exception ex)
+                    {
+                        mensagem = $"Erro ao executar API Eco: {ex}";
+                        erroExecucao = true;
+                    }
+                }
+                );
+            }
+            catch (Exception Ex)
+            {
+                mensagem = $"Erro: job averbação cartão: {Ex}";
+                erroExecucao = true;
+            }
+            finally
+            {
+                mensagem = "finalizou job averbação cartão";
+            }
+
+            return Task.FromResult(mensagem);
+        }
+    }
+}
